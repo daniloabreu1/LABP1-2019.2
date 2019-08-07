026425343d166efa06177f0a3c455994aa8d8edc
Danilo Abreu <daniloabreu1@gmail.com>
2019-08-07 12:05:56 -0300

    Create aula01-3.c

---
+++ aula01/aula01-3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+main(){
+    system("color F1");
+    printf("********\t  ***   \t  *   \t    *     \n");
+    printf("*      *\t *   *  \t ***  \t   * *    \n");
+    printf("*      *\t*     * \t***** \t  *   *   \n");
+    printf("*      *\t*     * \t  *   \t *     *  \n");
+    printf("*      *\t*     * \t  *   \t*       * \n");
+    printf("*      *\t*     * \t  *   \t *     *  \n");
+    printf("*      *\t*     * \t  *   \t  *   *   \n");
+    printf("*      *\t *   *  \t  *   \t   * *    \n");
+    printf("********\t  ***   \t  *   \t    *     \n");
+}
