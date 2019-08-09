095a099d705aaf485935ef1484d3a402b490b712
Danilo Abreu <daniloabreu1@gmail.com>
2019-08-09 12:23:41 -0300

    ex02

---
+++ aula02/aula02-0.c
@@ -0,0 +1,6 @@
+#include <locale.h>
+#include <stdio.h>
+main(){
+  setlocale(LC_ALL, "Portuguese");
+  printf("OLÁ MUNDO. ÇUCEÇO\n");
+}
---
+++ aula02/aula02-8.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+main(){
+    float lado;
+    printf("DIGITE UM LADO DO QUADRADO ");
+    scanf("%f", &lado);
+    printf("A AREA DO QUADRADO %.2f",lado*lado);
+}
